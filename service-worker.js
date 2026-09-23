f509c1500729b4f989cb20105cbc516100959fc1
Potocki Eryk <potockieryk99@gmail.com>
2026-09-23 17:20:55 +0200

    fix: switch profile and shop exclusively; retain iOS startup fixes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'poker-zgadywany-' + self.registration.scope.replace(/[^a-z0-9]/gi, '_') + '-';
-const CACHE_NAME = CACHE_PREFIX + '639257496224034226';
+const CACHE_NAME = CACHE_PREFIX + '639257735584993350';
 const SHELL = [
   './',
   './index.html',
